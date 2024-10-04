b742c744ba267d507f348e198b87d534b133aded
Connor States <123701849+ConnorStates0@users.noreply.github.com>
2024-10-04 02:52:39 -0400

    recursive build done, modify token parser ,modify print although it gets the job done

--- A1.py
+++ A1.py
@@ -16,8 +16,9 @@ def read_lines_from_txt(fp: [str, os.PathLike]) -> List[str]:
     and newline characters.
     """
     # TODO
-    
-    return []
+    with open(fp) as file:
+        lines = [line.rstrip() for line in file]
+    return lines
 
 
 def is_valid_var_name(s: str) -> bool:
@@ -30,7 +31,9 @@ def is_valid_var_name(s: str) -> bool:
     for char in s:
         if char not in var_chars:
             return False
-    if s[0] in alphabet_chars:
+    if s[0] in numeric_chars:
+        return False
+    elif s[0] in alphabet_chars:
         return True
     return False
 
@@ -62,7 +65,15 @@ class ParseTree:
 
     def print_tree(self, node: Optional[Node] = None, level: int = 0) -> None:
         # TODO
-        print("")
+        print("") # ---- per level
+        print('_'.join(self.root.elem))
+        children = self.root.children
+        for child in children:
+            print('----'+"_".join(child.elem))
+            for grandkid in child.children:
+                print('--------'+"_".join(grandkid.elem))
+                for greatgrandkid in grandkid.children:
+                    print('------------'+"_".join(greatgrandkid.elem))
 
 
 
@@ -81,8 +92,75 @@ def parse_tokens(s_: str, association_type: Optional[str] = None) -> Union[List[
 
     s = s_[:]  #  Don't modify the original input string
     # TODO
-
-    return []
+    if s == "":
+        print("Token Empty")
+        return False
+    else:
+        finalStrList = []
+        i = 0
+        dotCount = 0
+        openBrackets = []
+        closedBrackets = []
+        for i in range(len(s)):
+            if s[i] in var_chars:
+                j = i
+                if i < len(s)-1:
+                    if s[i+1] not in var_chars:
+                        if is_valid_var_name(s[j:i+1]):
+                            finalStrList.append(s[j:i+1])
+                        else:
+                            print("invalid variable name at index "+str(i))
+                            return False
+                else:
+                    if is_valid_var_name(s[j:]):
+                            finalStrList.append(s[j:])
+                    else:
+                        print("invalid variable name at index " + str(i))
+                        return False
+            elif s[i] == ' ':
+                i = i + 1
+            elif s[i] not in all_valid_chars:
+                            print('Invalid Character '+s[j]+' encountered at index '+str(j))
+                            return False 
+            elif s[i] == '\\':
+                if s[i+1] in alphabet_chars:
+                    finalStrList.append('\\')
+                elif s[i+1] == ' ':
+                    print('Invalid space inserted after \\ at index '+str(i))
+                    return False
+                else:
+                    print('Backslashes not followed by a variable name at '+str(i))
+                    return False
+            elif s[i] == '.':
+                if s[i-1] in alphabet_chars:
+                    count = 1
+                    while s[i+count] == ' ':
+                        count = count + 1
+                    if s[i+count] in alphabet_chars or s[i+1] =='(' or s[i+1] =='\\':
+                        finalStrList.append('(')
+                        dotCount = dotCount + 1
+                    else:
+                        print('Invalid Character following dot at index '+str(i))
+                        return False
+                else:
+                    print("Must have a variable name before character '.' at index "+str(i))
+                    return False
+            elif s[i] == '(':
+                finalStrList.append('(')
+                openBrackets.append(i)
+            elif s[i] == ')':
+                finalStrList.append(')')
+                closedBrackets.append(i)
+        if len(openBrackets) < len(closedBrackets):
+            print("Bracket ) at index: "+str(closedBrackets[-1])+" not matched with a opening bracket '('")
+            return False
+        elif len(openBrackets) > len(closedBrackets):
+            print("Bracket ( at index: "+str(openBrackets[-1])+" not matched with a closing bracket ')'")
+            return False
+        if dotCount > 0:
+            for i in range(dotCount):
+                finalStrList.append(')')
+    return finalStrList
 
 
 def read_lines_from_txt_check_validity(fp: [str, os.PathLike]) -> None:
@@ -146,7 +224,39 @@ def build_parse_tree_rec(tokens: List[str], node: Optional[Node] = None) -> Node
     """
 
     #TODO
-    return Node()
+    if node == None:
+        root = Node(tokens)
+    else:
+        root = node
+    i = 0
+    while i < len(tokens):
+        if tokens[i] == '(':
+                j = i+1
+                closeCount = 1
+                while closeCount > 0 and j < len(tokens):
+                    if tokens[j] == '(':
+                        closeCount = closeCount + 1
+                    elif tokens[j] == ')':
+                        closeCount = closeCount - 1
+                    j = j + 1
+                new_node = Node(tokens[i:j])
+                new_node1 = Node(tokens[i])
+                new_node2 = Node(tokens[j-1])
+                new_node.add_child_node(new_node1)
+                build_parse_tree_rec(tokens[i+1:j-1], new_node)
+                new_node.add_child_node(new_node2)
+                root.add_child_node(new_node)
+                
+                i = j-1
+        elif tokens[i] == '\\':
+            root.add_child_node(Node([tokens[i]]))
+            i = i + 1
+        elif tokens[i] == ')':
+            i = i+1
+        else:
+            root.add_child_node(Node([tokens[i]]))
+            i = i + 1
+    return root
 
 
 def build_parse_tree(tokens: List[str]) -> ParseTree:
@@ -166,14 +276,14 @@ if __name__ == "__main__":
     read_lines_from_txt_output_parse_tree(valid_examples_fp)
 
     print("Checking invalid examples...")
-    read_lines_from_txt_check_validity(invalid_examples_fp)
+    #read_lines_from_txt_check_validity(invalid_examples_fp)
 
     # Optional
-    print("\n\nAssociation Examples:")
-    sample = ["a", "b", "c"]
-    print("Right association")
-    associated_sample_r = add_associativity(sample, association_type="right")
-    print(associated_sample_r)
-    print("Left association")
-    associated_sample_l = add_associativity(sample, association_type="left")
-    print(associated_sample_l)
+    #print("\n\nAssociation Examples:")
+    #sample = ["a", "b", "c"]
+    #print("Right association")
+    #associated_sample_r = add_associativity(sample, association_type="right")
+    #print(associated_sample_r)
+    #print("Left association")
+    #associated_sample_l = add_associativity(sample, association_type="left")
+    #print(associated_sample_l)
